c4a67a390ec2f9d21ba5b2497d770b552baee7b6
Hannah Fisher <hannahtfisher@gmail.com>
2026-04-28 10:18:50 +0100

    Added pipe destroyer for when pipes go off screen

---
+++ Assets/Scripts/PipeDestroyer.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+public class PipeDestroyer : MonoBehaviour
+{
+    // TODO create one screenboundary in GameManager when created and get from there? Avoids repeated variable
+    private float m_screenBoundaryX = 14f;
+    
+    private void Update()
+    {
+        if (transform.position.x < -m_screenBoundaryX)
+        {
+            DestroyPipe();
+        }
+    }
+
+    private void DestroyPipe()
+    {
+       Destroy(gameObject); 
+    }
+}
