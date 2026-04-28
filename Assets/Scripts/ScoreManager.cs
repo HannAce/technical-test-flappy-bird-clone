9f532cb4cb80227eed69b2c1e06c32f3b03ed91c
Hannah Fisher <hannahtfisher@gmail.com>
2026-04-28 13:55:43 +0100

    Replaced direct references to score with an event

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 public class ScoreManager : MonoBehaviour
 {
     private int m_score;
-    [SerializeField] private ScoreUI m_scoreUI;
-    [SerializeField] private ScoreUI m_finalScoreUI;
+    // Replaced this with event, leaving for thought process
+    //[SerializeField] private ScoreUI m_scoreUI;
+    //[SerializeField] private ScoreUI m_finalScoreUI;
     
     public static ScoreManager Instance;
+
+    public Action<int> OnScoreChanged;
 
     private void Awake()
     {
@@ -22,9 +25,10 @@ public class ScoreManager : MonoBehaviour
     public void AddScore()
     {
         m_score++;
-        // TODO try to make an event for updating score which ScoreUI can subscribe to?
-        m_scoreUI.UpdateScoreUI(m_score);
-        m_finalScoreUI.UpdateScoreUI(m_score);
+        OnScoreChanged?.Invoke(m_score);
+        // Replaced this with event, leaving for thought process
+        //m_scoreUI.UpdateScoreUI(m_score);
+        //m_finalScoreUI.UpdateScoreUI(m_score);
         Debug.Log($"[ScoreManager] Score: {m_score}");
     }
 }
--- Assets/Scripts/ScoreUI.cs
+++ Assets/Scripts/ScoreUI.cs
@@ -7,9 +7,21 @@ public class ScoreUI : MonoBehaviour
     [SerializeField] private TMP_Text m_scoreText;
     [SerializeField] private bool m_isFinalScore;
 
+    private ScoreManager m_scoreManager;
+
     private void Start()
     {
+        m_scoreManager = ScoreManager.Instance;
+        m_scoreManager.OnScoreChanged += UpdateScoreUI;
+        
         UpdateScoreUI(0);
+    }
+
+    private void OnDestroy()
+    {
+        if (m_scoreManager != null) {
+            m_scoreManager.OnScoreChanged -= UpdateScoreUI;
+        }
     }
 
     public void UpdateScoreUI(int score)
