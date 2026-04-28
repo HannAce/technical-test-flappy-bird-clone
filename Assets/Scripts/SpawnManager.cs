27d04cc36b0a57a62fa6f9dfe5a17ceaf75bfac7
Hannah Fisher <hannahtfisher@gmail.com>
2026-04-28 11:54:26 +0100

    Randomised pipe spawn height

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,15 +1,16 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject m_pipes;
-
     [SerializeField] private float m_SpawnDelay;
     
-    // TODO change hard coded screen boundary to use Screen.width?
+    // TODO change hard coded screen boundary to use Screen.width/height?
     private float m_screenboundary = 14f;
+    private float m_pipeSpawnHeightRange = 3.5f;
     
 
     private void Start()
@@ -26,9 +27,14 @@ public class SpawnManager : MonoBehaviour
 
     private void SpawnPipe()
     {
-        Vector3 spawnPosition = new Vector3(m_screenboundary, 0, 0);
+        Vector3 spawnPosition = new Vector3(m_screenboundary, RandomPipeHeight(), 0);
 
         Instantiate(m_pipes, spawnPosition, Quaternion.identity);
+    }
+
+    private float RandomPipeHeight()
+    {
+        return Random.Range(-m_pipeSpawnHeightRange, m_pipeSpawnHeightRange);
     }
     
 }
